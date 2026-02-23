729c60ba5a8363afd4097dc14d2544fc28375585
IuryRezende <iuryrezende5098@gmail.com>
2026-02-23 18:33:07 -0300

    Added the function to input the name of player and choose the class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,27 +3,59 @@ package org.example;
 import org.example.Armory.*;
 import org.example.Classes.*;
 import org.example.SystemTools.SystemCombat;
+import org.example.model.Player;
 
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     private static String formatName(String name){
         return name.substring(0, Math.min(10, name.length()));
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        try {
-//            System.out.println("Enter the name of P1: ");
-//            String p1Name = formatName(sc.nextLine());
-//
-//            System.out.println("Enter the name of P2: ");
-//            String p2Name = formatName(sc.nextLine());
+    private static int chooseClass(){
+        System.out.println("Choose the class to play");
+        System.out.println("1-Mage    2-Barbarian   3-Assassin");
+        byte choosedClass = sc.nextByte();
+        sc.nextLine();
+        if (choosedClass < 1 || choosedClass > 3){
+            System.out.println("Invalid argument, try again");
+            chooseClass();
+        }
 
-            BarbarianClass p1 = new BarbarianClass("Maradonna", 4, 4, 4);
-            MageClass p2 = new MageClass("CR7", 4, 3, 3);
+        System.out.println("Are you sure? (y/n)");
+        char areSure = sc.nextLine().charAt(0);
+
+        return areSure == 'y' ? choosedClass : chooseClass();
+
+    };
+
+    private static Player createClass(String name){
+        int arquetype = chooseClass();
+
+        if(arquetype == 1) {
+            return new MageClass(name, 3, 3, 4);
+        } else if(arquetype == 2) {
+            return new BarbarianClass(name, 4, 3, 3);
+        } else {
+            return new AssassinClass(name, 3, 4, 3);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Enter the name of P1: ");
+            String p1Name = formatName(sc.nextLine());
+            Player p1 = createClass(p1Name);
+
+
+            System.out.println("Enter the name of P2: ");
+            String p2Name = formatName(sc.nextLine());
+            Player p2 = createClass(p2Name);
+
 
             SystemCombat.startCombat(p1, p2);
         } catch (RuntimeException e) {
